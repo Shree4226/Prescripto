07b41e7fec28c319324b2853fe0761c24321d426
Shreya Kumari <shreesim4226@gmail.com>
2026-01-20 20:28:26 +0530

    Implemented about us and contact us pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt=''/>
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600 '>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quos quis iste quo quasi. Cumque voluptatem officiis obcaecati aspernatur vitae? Deleniti, architecto sed nam quidem suscipit facere enim ex aperiam. Vel tempore voluptas enim qui, cumque impedit, delectus repellendus adipisci architecto, corrupti est fugit eum ullam. Ut commodi molestias accusantium?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam et explicabo tempore asperiores, autem at doloribus facilis perspiciatis quo odio, ullam harum laboriosam omnis, pariatur enim rerum. Recusandae, accusantium optio.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid totam sint voluptate quis quibusdam ipsa vel iusto. Obcaecati, fugiat ullam!</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-(--primary) hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-(--primary) hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convinience:</b>
+          <p>Access to a network of trusted healthcare professionls in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-(--primary) hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state,setState] = useState('Sign up')
+
+  const [email,setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSubmitHandler = async (event)=>{
+    event.preventDefault()
+  }
   return (
-    <div>Login</div>
+    
   )
 }
 
