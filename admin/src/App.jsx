df71dd3e4f29e4bb9c254808d657631f8c1c2587
Shreya Kumari <shreesim4226@gmail.com>
2026-01-23 12:52:43 +0530

    Implemented sidebar and add doctor feature of admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,11 @@ import { useContext } from 'react';
 import { AdminConext } from './context/AdminContext';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-
+import {Route, Routes} from 'react-router-dom'
+import Dashboard from './pages/admin/Dashboard'
+import AllAppointments from './pages/admin/AllAppointments';
+import AddDoctor from './pages/admin/AddDoctor'
+import DoctorsList from './pages/admin/DoctorsList';
 const App = () => {
 
   const {aToken} = useContext(AdminConext)
@@ -16,6 +20,13 @@ const App = () => {
       <Navbar/>
       <div className='flex items-start'>
         <Sidebar/>
+        <Routes>
+          <Route path ='/' element ={<></>}/>
+          <Route path = '/admin-dashboard' element={<Dashboard/>}/> 
+          <Route path='/all-appointments'element={<AllAppointments/>}/>
+          <Route path='/add-doctor'element={< AddDoctor />}/>
+          <Route path='/doctor-list'element={< DoctorsList />}/>
+        </Routes>
       </div>
     </div>
   ):(
