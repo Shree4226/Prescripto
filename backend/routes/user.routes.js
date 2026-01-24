6fcc708d1d3e32babc1e907602af18e30de5b8db
Shreya Kumari <shreesim4226@gmail.com>
2026-01-24 20:42:42 +0530

    Implemented api to appointment list and cancel appointments

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, loginUser, getProfile, updateProfile,bookAppointment, listAppointment} from '../controllers/user.controllers.js'
+import { registerUser, loginUser, getProfile, updateProfile,bookAppointment, listAppointment, cancelAppointment} from '../controllers/user.controllers.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -11,5 +11,6 @@ userRouter.get('/get-profile', authUser ,getProfile)
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment)
 userRouter.get('/appointments', authUser ,listAppointment)
+userRouter.post('/cancel-appointment',authUser,cancelAppointment)
 
 export default userRouter
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -7,7 +7,7 @@ import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
-  const {backendUrl, token} = useContext(AppContext)
+  const {backendUrl, token, getDoctorsData} = useContext(AppContext)
 
   const [appointments, setAppointments] = useState([])
   const months = [" ","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
@@ -24,6 +24,23 @@ const MyAppointments = () => {
         setAppointments(data.appointments.reverse())
         console.log(data.appointments)
       }
+    }catch(error){
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  const cancelAppointment = async (appointmentId)=>{
+    try{
+       const {data} = await axios.post(backendUrl+'/api/user/cancel-appointment',{appointmentId},{headers:{token}})
+
+       if(data.success){
+        toast.success(data.message)
+        getUserAppointments()
+        getDoctorsData()
+       }else{
+        toast.error(data.message)
+       }
     }catch(error){
       console.log(error)
       toast.error(error.message)
@@ -55,8 +72,9 @@ const MyAppointments = () => {
           </div>
           <div></div>
           <div className='flex flex-col gap-2 justify-end'>
-            <button className='text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-(--primary) hover:text-white transition-all duration-300'>Pay Online</button>
-            <button className='text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+            {!item.cancelled && <button className='text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-(--primary) hover:text-white transition-all duration-300'>Pay Online</button>}
+            {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+            {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
           </div>
         </div>
         ))}
